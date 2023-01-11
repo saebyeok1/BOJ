dbcb026bbf24fd5c6807252369287c779b9c51bd
saebyeok1 <qhdid3734@gmail.com>
2023-01-12 03:58:17 +0900

    feat: BOJ 8393

---
+++ day3/n8393.java
@@ -0,0 +1,15 @@
+package day3;
+
+import java.util.Scanner;
+public class n8393 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= a; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
