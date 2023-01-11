bf4172d74a6f9fffb9552c81829b5a53a5fa152d
saebyeok1 <qhdid3734@gmail.com>
2023-01-12 03:58:30 +0900

    feat: BOJ 10430

---
+++ day3/n10430.java
@@ -0,0 +1,16 @@
+package day3;
+
+import java.util.Scanner;
+public class n10430 {
+    public static void main(String[] args) {
+        Scanner nsc = new Scanner(System.in);
+        int A = nsc.nextInt();
+        int B = nsc.nextInt();
+        int C = nsc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
