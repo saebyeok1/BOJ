bb8970803739998b342ba2d9713ad07e9f756ebe
saebyeok1 <qhdid3734@gmail.com>
2023-01-11 21:57:36 +0900

    feat: example

---
+++ day3/example.java
@@ -0,0 +1,12 @@
+package day3;
+
+import java.util.Scanner;
+public class example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
