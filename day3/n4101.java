8c996b4071e201927e699e474a4d758be60d62fd
saebyeok1 <qhdid3734@gmail.com>
2023-01-12 03:58:05 +0900

    feat: BOJ 4101

---
+++ day3/n4101.java
@@ -0,0 +1,22 @@
+package day3;
+
+import java.util.Scanner;
+public class n4101 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+            if(a>b){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        }
+    }
+}
