df3f98d7b567c71ba236ca32de0cb5d75baa78b9
saebyeok1 <qhdid3734@gmail.com>
2023-01-13 04:38:37 +0900

    feat: BOJ 10926

---
+++ Day4/n10926.java
@@ -0,0 +1,13 @@
+package Day4;
+
+import java.util.Scanner;
+public class n10926 {
+    public static void main(String[] args) {
+        Scanner nsc = new Scanner(System.in);
+
+        String name = nsc.nextLine();
+
+        System.out.println(name+"??!");
+
+    }
+}
